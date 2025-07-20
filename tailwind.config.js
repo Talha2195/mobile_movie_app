fb8fb87dfef0f642cc1a8e84aa36ead8ae34bdb0
Talha2195 <talha.asif0010@gmail.com>
2025-07-20 13:07:50 +0300

    Added bottom navigation bar with styling and routes, changed app icon, added assets and images, made functional component for tab info

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,12 @@ module.exports = {
         secondary: "#151312",
         light: {
           100: 'f9f9f9',
+          200: '#A8B5DB',
+          300: '#9CA4AB',
+        },
+        dark :{
+          100: '#221f3d',
+          200: '#0f0d23',   
         }
       }
     },
